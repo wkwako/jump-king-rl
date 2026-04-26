9af123a0546533cf48efdc43ef1498d42646ca19
William Kwako <wkwako3@gatech.edu>
2026-04-25 21:30:41 -0500

    first draft finished. untested

--- JumpKingEnv.py
+++ JumpKingEnv.py
@@ -13,15 +13,18 @@ from gymnasium.error import DependencyNotInstalled
 class JumpKingEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
     def __init__(self):
-        #define action space here, tuples of buttons with presses for each
-        #pass into function that activates it via pydirectinput
-        self.action_space = self.init_action_space()
-        self.state = None #TODO: set this
-        self.action_map = {}
+        self.action_map = self.init_action_map()
+        self.action_space = spaces.Discrete(len(self.action_map))
+        self.state = None
         self.gamedata = None
-        self.gamadata_prev
+        self.gamedata_prev = None
         self.new_screen_reward = 300
+        self.same_level_reward = 5
         self.jumped = False
+
+        self.observation_space = spaces.Box(low=np.array([-np.inf, -np.inf, -np.inf, -np.inf]),
+            high=np.array([np.inf, np.inf, np.inf, np.inf]), dtype=np.float32
+)
 
     def step(self, action):
         reward = 0
@@ -33,7 +36,6 @@ class JumpKingEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         self.execute_action(action)
 
         #reads gamedata
-        self.gamedata = None
         self.gamedata = self.read_gamedata()
         x, y, vel_x, vel_y, is_on_ground, current_screen, total_screens, jump_frames, jump_percentage, max_height_this_jump = self.gamedata   
         x_prev, y_prev, vel_x_prev, vel_y_prev, is_on_ground_prev, current_screen_prev, total_screens_prev, jump_frames_prev, jump_percentage_prev, max_height_this_jump_prev = self.gamedata_prev
@@ -43,30 +45,25 @@ class JumpKingEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         if current_screen > current_screen_prev:
             reward += self.new_screen_reward
 
-        #if we landed higher, moderate reward. if it was a max jump, bonus reward
-        if y > y_prev:
+        #if we landed higher, moderate reward. if it was a max jump, bonus reward. or punishes falling
+        if y > y_prev or y < y_prev:
             reward += self.new_height_reward(y, y_prev, jump_percentage)
 
-        if y == y_prev:
-            pass
+        #if we jumped and stayed at the same y level, small reward
+        # if self.jumped and y == y_prev:
+        #     reward += 5
         
-        #reward screen transitions the most
-        #reward full jumps that land higher more
-        #reward jumps that land higher more
-        #reward jumps that land at the same height but in a different location
-        #punish falling
+        #reward jumps that land at the same height but in a different location?
         #reward moving right after a big fall so the player stands up?
 
-        #define a function that sets termination bool
+        #define state
+        self.state = (x, y, vel_x, vel_y)
 
-        #define state with location, etc.
-
-        #terminated is always True since an episode is one jump?
-        #not necessarily. action could just be walking
-
+        #if we jumped, terminate episode
         if self.jumped:
             terminated = True
         
+        #if we walked, continue episode
         else:
             terminated = False
 
@@ -76,32 +73,41 @@ class JumpKingEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         #state, reward, if the episode is terminated, truncation, and info dict
         return np.array(self.state, dtype=np.float32), reward, terminated, False, {}
     
+    def reset(self):
+        self.gamedata = self.read_gamedata()
+        self.gamedata_prev = list(self.gamedata)
+
+        x, y, vel_x, vel_y = self.gamedata[:4]
+        self.state = (x, y, vel_x, vel_y)
+
+        return np.array(self.state, dtype=np.float32), {}
+
     def new_height_reward(self, y, y_prev, jump_percentage, max_jump_bonus=1.20):
         #if jump was max height, increase reward by 20%
 
         reward = y - y_prev
 
-        if jump_percentage == 1:
+        if jump_percentage == 1 and y > y_prev:
             reward *= max_jump_bonus
 
         return reward
 
     def read_gamedata(self):
         """Reads the game state and outputs a list with 9 parts.
-           [x, y, velX, velY, isOnGround, currentScreen, totalScreens, chargeTimer, maxHeightThisJump]"""
+           [x, y, velX, velY, isOnGround, currentScreen, totalScreens, jumpFrames, jumpPercentage, maxHeightThisJump]"""
         while True:
             try:
                 with open("C:/Program Files (x86)/Steam/steamapps/workshop/content/1061090/3699885336/gamestate.txt") as f:
                     content = f.read()
                 parts = content.split(",")
                 if len(parts) == 10:
-                    return parts
+                    return [float(parts[0]), float(parts[1]), float(parts[2]), float(parts[3]), parts[4].strip().lower() == "true", int(parts[5]), int(parts[6]), int(parts[7]), float(parts[8]), float(parts[9])]
             except:
                 pass
                 
     def execute_action(self, action):
         #map action index to keypresses
-        left, right, jump = self.action_space[action]
+        left, right, jump = self.action_map[action]
 
         #walking
         if not jump:
@@ -127,7 +133,7 @@ class JumpKingEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
     
     def key_press(self, key, duration, key2=None):
-        #key2 is released first so directional jumps occur
+        #key is released first when key2 is present so directional jumps occur
         pydirectinput.keyDown(key)
 
         if key2:
@@ -135,68 +141,64 @@ class JumpKingEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
         time.sleep(duration)
 
+        pydirectinput.keyUp(key)
+
         if key2:
             pydirectinput.keyUp(key2)
 
-        pydirectinput.keyUp(key)
-
-    def init_action_space(self):
+    def init_action_map(self):
         #left, right, spacebar
-        action_space = []
+        action_map = []
 
         #walk left
-        action_space.append([0.2, 0, 0])
+        action_map.append([0.2, 0, 0])
 
         #walk small left
-        action_space.append([0.1, 0, 0])
+        action_map.append([0.1, 0, 0])
 
         #walk right
-        action_space.append([0, 0.2, 0])
+        action_map.append([0, 0.2, 0])
 
         #walk small right
-        action_space.append([0, 0.1, 0])
+        action_map.append([0, 0.1, 0])
 
         #jump up
-        action_space.append([0, 0, 0.6])
+        action_map.append([0, 0, 0.6])
 
         #jump right, 0.1s
-        action_space.append([0, 0.1, 0.1])
+        action_map.append([0, 0.1, 0.1])
 
         #jump right, 0.2s
-        action_space.append([0, 0.2, 0.2])
+        action_map.append([0, 0.2, 0.2])
 
         #jump right, 0.3s
-        action_space.append([0, 0.3, 0.3])
+        action_map.append([0, 0.3, 0.3])
 
         #jump right, 0.4s
-        action_space.append([0, 0.4, 0.4])
+        action_map.append([0, 0.4, 0.4])
 
         #jump right, 0.5s
-        action_space.append([0, 0.5, 0.5])
+        action_map.append([0, 0.5, 0.5])
 
         #jump right, 0.6s
-        action_space.append([0, 0.6, 0.6])
+        action_map.append([0, 0.6, 0.6])
 
         #jump left, 0.1s
-        action_space.append([0.1, 0, 0.1])
+        action_map.append([0.1, 0, 0.1])
 
         #jump left, 0.2s
-        action_space.append([0.2, 0, 0.2])
+        action_map.append([0.2, 0, 0.2])
 
         #jump left, 0.3s
-        action_space.append([0.3, 0, 0.3])
+        action_map.append([0.3, 0, 0.3])
 
         #jump left, 0.4s
-        action_space.append([0.4, 0, 0.4])
+        action_map.append([0.4, 0, 0.4])
 
         #jump left, 0.5s
-        action_space.append([0.5, 0, 0.5])
+        action_map.append([0.5, 0, 0.5])
 
         #jump left, 0.6s
-        action_space.append([0.6, 0, 0.6])
+        action_map.append([0.6, 0, 0.6])
 
-        return action_space
-    
-    def init_states(self):
-        #self.x, self.y, self.
-        pass
+        return action_map
--- JumpKingRL.py
+++ JumpKingRL.py
@@ -1,4 +1,46 @@
+import math
+from typing import Optional, Union
+import time
+import keyboard
+import pydirectinput
+import numpy as np
+import signal
+import os
+
 import gymnasium as gym
 from stable_baselines3 import PPO
-import numpy as np
 
+import JumpKingEnv
+
+def save_and_quit():
+    print("Saving model and quitting...")
+    model.save(MODEL_NAME)
+    exit()
+
+MODEL_NAME = "jumpking_ppo"
+
+env = JumpKingEnv()
+
+#if model exists, load it
+if os.path.exists(MODEL_NAME + ".zip"):
+    print ("Loading existing model...")
+    model = PPO.load(MODEL_NAME, env=env)
+
+#if it doesn't, create a new model
+else:
+    print ("Creating new model...")
+    model = PPO("MlpPolicy", env, verbose=1)
+
+#save and quit if q is pressed
+keyboard.add_hotkey("ctrl+q", save_and_quit)
+
+try:
+    model.learn(total_timesteps=20)
+    print ("Training complete. Saving...")
+    model.save(MODEL_NAME)
+
+except KeyboardInterrupt:
+    print ("Interrupted. Saving...")
+    model.save(MODEL_NAME)
+
+env.close()
